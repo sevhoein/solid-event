2a3a1596fc6a16a19ade3e7582d4f8fabfb0d8de
Severin <severin.hoein@gmail.com>
2019-10-23 11:35:45 +0200

    updated Files to php

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" href="css/style.css">
+  </head>
+  <body>
+
+      <main>
+          
+          <header>
+              <?php include 'nav.php' ?>
+          </header>
+          
+          <section>
+              
+          </section>
+          
+          <section>
+          </section>
+          
+          <section>
+          </section>
+          
+          <section>
+          </section>
+          
+          <section>
+          </section>
+      
+      </main>
+    <script src="js/script.js"></script>
+  </body>
+</html>
---
+++ nav.php
@@ -0,0 +1,11 @@
+<nav>
+    <ul>
+        <li><a href="Abc">unstoppable</a></li>
+        <li><a href="def">Shine Conference</a></li>
+        <li><a href="hij">Workshops</a></li>
+        <li><a href="oihj">ReferentInnen</a></li>
+        <li><a href="oiwn">Programm</a></li>
+        <li><a href="wnf">Preise & Anmeldung</a></li>
+        <li><a href="wnf">Kontakt</a></li>
+    </ul>
+</nav>
